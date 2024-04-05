cab4e49b06a0461b2c2e45dc1b675b7e4ae67c9a
unknown <21-45612-3@student.aiub.edu>
2024-04-05 15:38:01 +0600

    update

--- view/InventoryItemUser.php
+++ view/InventoryItemUser.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,63 +8,60 @@
 </head>
 <body class="container my-5">
    <h2>List of Items</h2> 
-     
+    
     <br>
     <table class="table">
-<thead>
-<tr>
-<th>ID</th>
-<th>Name</th>
-<th>Description</th>
-<th>IMG</th>
-<th>Quantity</th> 
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>IMG</th>
+                <th>Quantity</th> 
+            
+            </tr>
+        </thead>
 
-</tr>
-</thead>
+        <tbody>
+            <?php
+            include("../model/db.php");
+            $con = connection();
 
-<tbody>
-    <?php
-    
-    include("../model/db.php");
-    $con = connection();
+            // Check connection
+            if ($con->connect_error) {
+                die("Connection failed: " . $con->connect_error);
+            }
 
-    //check connection
-    if($con->connect_error){
-        die("Connection failed: ".$con->connect_error);
-    }
+            // Read all records from DB table
+            $sql = "SELECT * FROM items";
+            $result = $con->query($sql);
 
-    //read all from DB table
-    $sql = "SELECT * FROM items";
-    $result = $con->query($sql);
+            if (!$result) {
+                die("Invalid query: " . $con->error);
+            }
 
-    if(!$result){
-        die("Invalid query: " . $con->error);
-    }
-
-    //read data for each row
-    while($row = $result->fetch_assoc()){
-
-        echo "
-        <tr>
-        <td>{$row['id']}</td>
-        <td>{$row['name']}</td>
-        <td>{$row['description']}</td>
-        <td>{$row['img_path']}</td>
-        <td>{$row['quantity']}</td>
-        <td>
-        
-
-        </td>
-        </tr>";
-    }
-    ?>
-   
-</tbody>
+            // Display data for each row
+            while ($row = $result->fetch_assoc()) {
+                ?>
+                <tr>
+                    <td><?php echo $row['id']; ?></td>
+                    <td><?php echo $row['name']; ?></td>
+                    <td><?php echo $row['description']; ?></td>
+                    <td><img src="<?php echo $row['img_path']; ?>" width="200" height="150"></td>
+                    <td><?php echo $row['quantity']; ?></td>
+                    <td>
+                       
+                    </td>
+                </tr>
+                <?php
+            }
+            ?>
+        </tbody>
     </table>
 
     <div class="col-sm-3 d-grid">
-            <a class="btn btn-outline-primary" href="userHome.php" role="button">Go Back</a>
-        </div>
+        <a class="btn btn-outline-primary" href="adminPanel.php" role="button">Go Back</a>
+    </div>
     
 </body>
 </html>
